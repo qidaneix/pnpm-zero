cffeb655a0261cf522917258101bd7af9c4552cc
jin.xiao <jin.xiao@shopee.com>
2022-02-07 00:06:48 +0800

    lerna and yarn workspace

--- src/index.js
+++ src/index.js
@@ -5,7 +5,7 @@ import { App } from "pnpm-zero-yarn-pkg";
 reactDom.render(
   <>
     <h1>Hello World</h1>
-    <App />
+    <App arr={[1, 2, 3, 4, 5]} />
   </>,
   document.getElementById("app")
 );
